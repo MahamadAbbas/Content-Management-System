31a60947d4d230a97c8133284ada73f5df505014
MahamadAbbas <mahamadabbas07@gmail.com>
2024-03-27 14:12:35 +0530

    finding unique user

---
+++ src/main/java/com/example/cms/exception/UserNotFoundByIdException.java
@@ -0,0 +1,12 @@
+package com.example.cms.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@SuppressWarnings("serial")
+public class UserNotFoundByIdException extends RuntimeException {
+	
+	private String message;
+}
--- src/main/java/com/example/cms/utility/ApplicationExceptionHandler.java
+++ src/main/java/com/example/cms/utility/ApplicationExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.example.cms.exception.UserAlreadyExistByEmailException;
+import com.example.cms.exception.UserNotFoundByIdException;
 
 import lombok.AllArgsConstructor;
 
@@ -31,4 +32,13 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
 		return errorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), 
 				"User Already exists with the given email ID");
 	}
+	
+	@ExceptionHandler
+	public ResponseEntity<ErrorStructure<String>> handleUserNotFoundById(
+			UserNotFoundByIdException ex){
+				return errorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(),
+						"User NOT FOUND by given ID");
+		
+	}
+	
 }
